f423d99e789fa9677f3e64c20e3f03b1f4f6aca5
Qingyu216 <m18911615202@163.com>
2017-01-18 13:49:57 +0800

    modify

---
+++ js/1.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    autoplay:3000,
+    autoplayDisableOnInteraction : false,
+    paginationClickable :true
+    
+  })  
